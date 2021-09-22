673a77936dd24b5a4da2848e3c802f0e1010b0d4
Nerower <p0sirega121234@gmail.com>
2021-09-22 14:28:28 +0300

    install .bat file created

---
+++ install.bat
@@ -0,0 +1,24 @@
+@echo off
+
+echo Start installing Windows Terminal Config...
+
+@REM SET PATH VALUES
+set path = %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe
+set settings_path = %path%\LocalState
+set assets_path = %path%\RoamingState
+
+echo Ready !
+
+@REM COPY ASSETS INFO
+xcopy /s assets/icons %assets_path%
+xcopy /s assets/wallpapers %assets_path%
+
+echo Assets successfully loaded !
+
+@REM COPY SETTINGS CONFIG
+xcopy settings.json %settings_path%
+
+echo Config successfully loaded !
+echo Done !
+
+@echo on
